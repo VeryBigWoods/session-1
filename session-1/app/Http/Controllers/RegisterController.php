c103ff5d8136fc629c45eddad2620cfd6cceb4c9
wyang14 <weili.yang@verizonmedia.com>
2021-03-29 22:40:17 +0100

    feature: JSON API - user registration and oauth token

---
+++ session-1/app/Http/Controllers/RegisterController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        try {
+            $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|max:255|unique:users',
+                'password' => 'required|string|min:8',
+            ]);
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            return ['user' => $user];
+        } catch (\Exception $ex) {
+            return response('', 400);
+        }
+    }
+}
