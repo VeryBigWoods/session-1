659a4015e53e05c7d258754d0795c9a03ccd3c24
wyang14 <weili.yang@verizonmedia.com>
2021-04-12 20:46:27 +0100

    livewire installation

--- session-1/routes/api.php
+++ session-1/routes/api.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/register', [RegisterController::class, 'register']);
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
