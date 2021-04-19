270e1e04a5c9b8609e7defc52423b371e98e7fe7
wyang14 <weili.yang@verizonmedia.com>
2021-04-19 21:03:10 +0100

    feature: due date and time on todo items

--- session-1/resources/views/livewire/create-todo.blade.php
+++ session-1/resources/views/livewire/create-todo.blade.php
@@ -6,6 +6,11 @@
             @error('name') <span class="error">{{ $message }}</span> @enderror
         </div>
 
+        <div class="mt-4">
+            <x-jet-label for="due" value="{{ __('Due') }}" />
+            <x-datetime-picker wire:model="due" class="w-full rounded-md shadow-sm border-gray-300 focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+
         <div class="flex items-center justify-end mt-4">
             <x-jet-button class="ml-4">
                 {{ __('Save') }}
