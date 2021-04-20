263688dfec7e384a5922fdcbdc1e0e4486279145
wyang14 <weili.yang@verizonmedia.com>
2021-04-20 22:52:09 +0100

    formatting

--- session-1/resources/views/components/datetime-picker.blade.php
+++ session-1/resources/views/components/datetime-picker.blade.php
@@ -3,12 +3,8 @@
 ])
 
 <div wire:ignore>
-    <input 
-    x-data="{value: @entangle($attributes->wire('model')), instance: undefined}"
-     x-init="() => {
+    <input x-data="{value: @entangle($attributes->wire('model')), instance: undefined}" x-init="() => {
          $watch('value', value => instance.setDate(value, true))
          instance = flatpickr($refs.input, {{ $options }} );
-    }" 
-     x-ref="input" type="text" data-input
-        {{ $attributes }} />
+    }" x-ref="input" type="text" data-input {{ $attributes }} />
 </div>
